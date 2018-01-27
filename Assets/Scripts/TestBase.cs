7631b2e499f2572e441a82bdfcd96257359428a2
Harrand <theplumder@gmail.com>
2018-01-27 12:16:34 +0000

    Added TestBase class.

---
+++ Assets/Scripts/TestBase.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestBase
+{
+    private bool success;
+
+    TestBase()
+    {
+        Debug.Log("Constructing " + this.GetType().Name);
+        success = false;
+    }
+
+    ~TestBase()
+    {
+        Debug.Log("Destructing " + this.GetType().Name + " (" + (success ? "success)" : "fail)"));
+    }
+}
