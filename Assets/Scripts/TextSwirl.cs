ed06c03468240080b1c6f6cf7abb9e8d041a2d9b
Qw3rtyone <mathew.aswin@ymail.com>
2018-01-31 18:01:29 +0000

    The Menu
    First bits of the menu

---
+++ Assets/Scripts/TextSwirl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class TextSwirl : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+
+    string text; //GetComponentInChildren<Text>().text;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        text = this.GetComponentInChildren<Text>().text;
+        this.GetComponentInChildren<Text>().text = "⌘  " + text + "  ⌘";
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        this.GetComponentInChildren<Text>().text = text;
+    }
+}
