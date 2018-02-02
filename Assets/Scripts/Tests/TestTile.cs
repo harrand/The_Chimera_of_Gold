38739681904a3384846a377562267444d6f22709
Harrand <theplumder@gmail.com>
2018-02-02 12:03:48 +0000

    Added comments.

--- Assets/Scripts/Tests/TestTile.cs
+++ Assets/Scripts/Tests/TestTile.cs
@@ -2,12 +2,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * TestBoard - Completely tests the Board Functionalities.
+ */
+
 public class TestTile : TestBase
 {
     private Tile tileScript;
     private GameObject tileObject;
-	// Use this for initialization
-	public TestTile()
+    /**
+    * TestBoard
+    * @Author Harry Hollands
+    * Instantiates a new GameObject, attaches the Board script and ensures that the Board constructor modifies the name.
+    */
+    public TestTile()
     {
         var pair = Tile.Create(null, 0, 0);
         this.tileScript = pair.Key;
