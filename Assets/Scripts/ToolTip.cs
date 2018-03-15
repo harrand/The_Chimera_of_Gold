6d0bf0377d51991d81112d7581f00865d6505805
Qw3rtyone <mathew.aswin@ymail.com>
2018-03-15 18:45:42 +0000

    Tooltip functional

---
+++ Assets/Scripts/ToolTip.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ToolTip : MonoBehaviour {
+    
+    public static bool visible = false;
+    public GameObject popUp;
+    public void ShowToolTip()
+    {
+        //Debug.Log("enter");
+        if(!visible)
+        {
+            popUp.SetActive(true);
+            visible = true;
+              
+        }
+    }
+    public void HideToolTip()
+    {
+
+        //Debug.Log("exit");
+        if (visible)
+        {
+            popUp.SetActive(false);
+            visible = false;
+        }
+    }
+}
